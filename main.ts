3c1cabf239cc91232449a0c3bbfffda236d92c62
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-20 16:58:28 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,15 @@
+function L () {
+    xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 128)
+    xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 64)
+    xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CW, 64)
+    xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CCW, 128)
+    xiamiBoard.OLEDshowUserText("Turn left", 0, 0)
+}
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 12) {
-        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 128)
-        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 128)
-        xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CW, 128)
-        xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CCW, 128)
-        xiamiBoard.OLEDshowUserText("Forward", 0, 0)
+        F()
     } else if (receivedNumber == 8) {
-        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 128)
-        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CCW, 128)
-        xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CCW, 128)
-        xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CW, 128)
-        xiamiBoard.OLEDshowUserText("Backward", 0, 0)
+        B()
     } else if (receivedNumber == 14) {
         xiamiBoard.motorStop(MOTOR.M1)
         xiamiBoard.motorStop(MOTOR.M2)
@@ -18,21 +17,34 @@ radio.onReceivedNumber(function (receivedNumber) {
         xiamiBoard.motorStop(MOTOR.M4)
         xiamiBoard.OLEDshowUserText("Stop", 0, 0)
     } else if (receivedNumber == 4) {
-        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 128)
-        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 64)
-        xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CW, 64)
-        xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CCW, 128)
-        xiamiBoard.OLEDshowUserText("Turn left", 0, 0)
+        L()
     } else if (receivedNumber == 10) {
-        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 64)
-        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 128)
-        xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CW, 128)
-        xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CCW, 64)
-        xiamiBoard.OLEDshowUserText("Turn right", 0, 0)
+        R()
     } else {
         basic.showIcon(IconNames.SmallHeart)
     }
 })
+function F () {
+    xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 128)
+    xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 128)
+    xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CW, 128)
+    xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CCW, 128)
+    xiamiBoard.OLEDshowUserText("Forward", 0, 0)
+}
+function B () {
+    xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 128)
+    xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CCW, 128)
+    xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CCW, 128)
+    xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CW, 128)
+    xiamiBoard.OLEDshowUserText("Backward", 0, 0)
+}
+function R () {
+    xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 64)
+    xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 128)
+    xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CW, 128)
+    xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CCW, 64)
+    xiamiBoard.OLEDshowUserText("Turn right", 0, 0)
+}
 xiamiBoard.IR_callbackUser(function (IR) {
     basic.showString("" + (xiamiBoard.IR_read()))
     basic.pause(2000)

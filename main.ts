54ee4b79b8804b4a0a53546bc6fb9e2ec95a4989
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-20 16:02:10 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 12) {
-        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 255)
-        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 255)
+        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 255)
+        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CCW, 255)
         xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CW, 255)
         xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CW, 255)
+    } else if (receivedNumber == 8) {
+        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 255)
+        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 255)
+        xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CCW, 255)
+        xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CCW, 255)
     } else {
         basic.showIcon(IconNames.SmallHeart)
     }

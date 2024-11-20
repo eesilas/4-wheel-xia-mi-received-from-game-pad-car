333028b7102e350a2753d378334448a950bf4963
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-20 15:52:27 +0800

    Rectangular chassis hold Xia Mi board drive car by receive game pad control

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 12) {
+        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 255)
+        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 255)
+        xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CW, 255)
+        xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CW, 255)
+    } else {
+        basic.showIcon(IconNames.SmallHeart)
+    }
+})
+basic.showIcon(IconNames.Chessboard)
+radio.setGroup(121)
 basic.forever(function () {
 	
 })

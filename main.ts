97c44a92a442dc1b71e8649c58b15b6f9048f3a2
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-20 16:08:07 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,12 +9,20 @@ radio.onReceivedNumber(function (receivedNumber) {
         xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 255)
         xiamiBoard.motorRun(MOTOR.M3, DIRECTION.CCW, 255)
         xiamiBoard.motorRun(MOTOR.M4, DIRECTION.CCW, 255)
+    } else if (receivedNumber == 4) {
+        xiamiBoard.motorStop(MOTOR.M1)
     } else {
         basic.showIcon(IconNames.SmallHeart)
     }
 })
+xiamiBoard.IR_callbackUser(function (IR) {
+    basic.showString(convertToText(IR))
+})
+let IR = 0
+xiamiBoard.initXiaMiBoard()
 basic.showIcon(IconNames.Chessboard)
 radio.setGroup(121)
+xiamiBoard.OLEDclear()
 basic.forever(function () {
-	
+    IR = xiamiBoard.IR_read()
 })
